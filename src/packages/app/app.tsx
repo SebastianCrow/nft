5b95f122a34134d6f5c739abd98d158ec09418ec
Sebastian Crow <sebastian@sebastiancrow.com>
2023-02-18 18:25:27 +0100

    Rendering <Listings /> component

---
+++ src/packages/app/app.tsx
@@ -0,0 +1,6 @@
+import { FunctionComponent } from 'react';
+import { Listings } from './components/listings/listings.component';
+
+export const App: FunctionComponent = () => {
+  return <Listings />;
+};
---
+++ src/packages/app/components/listings/listings.component.tsx
@@ -0,0 +1,5 @@
+import { FunctionComponent } from 'react';
+
+export const Listings: FunctionComponent = () => {
+  return <div>Listings</div>;
+};
