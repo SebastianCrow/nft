e01443b78a3a8c42f448dbfbfdf60aa5e25270ec
Sebastian Crow <sebastian@sebastiancrow.com>
2023-02-19 20:22:22 +0100

    Cleanup

--- src/packages/app/components/listings/listings.component.tsx
+++ src/packages/app/components/listings/listings.component.tsx
@@ -1,14 +1,8 @@
-import {
-  FunctionComponent,
-  useEffect,
-  useState,
-  RefObject,
-  useMemo,
-} from 'react';
+import { FunctionComponent, useEffect, useState } from 'react';
 import { FixedSizeGrid } from 'react-window';
 import { Card, Input } from '../../../ui';
 import { useFetchListings } from '../../hooks/useFetchListings';
-import { classes, useIntersectionObserver } from '../../../../shared';
+import { classes, usePrevious } from '../../../../shared';
 import { useFilterListings } from '../../hooks/useFilterListings';
 
 export const Listings: FunctionComponent = () => {
@@ -21,30 +15,69 @@ export const Listings: FunctionComponent = () => {
     searchQuery,
   });
 
-  const listEndRefs: RefObject<HTMLDivElement>[] = useMemo(
-    () =>
-      new Array(20).fill({
-        // TODO
-        current: null,
-      }),
-    []
-  );
-
-  const listEnd = useIntersectionObserver({
-    refs: listEndRefs,
-  });
-
+  const [page, setPage] = useState(1);
+  const prevPage = usePrevious(page);
   useEffect(() => {
-    if (!isFetching && listEnd) {
+    if (!prevPage || page > prevPage) {
       fetchNextPage();
     }
-  }, [fetchNextPage, isFetching, listEnd]);
+  }, [fetchNextPage, page, prevPage]);
 
-  const Cell = ({ columnIndex, rowIndex, style }: any) => (
-    <div style={style}>
-      Item {rowIndex},{columnIndex}
-    </div>
-  );
+  const COLUMNS = 3;
+
+  const Cell = ({ rowIndex, columnIndex, style }: any) => {
+    const index = rowIndex * COLUMNS + columnIndex;
+    const item = filteredItems[index];
+
+    if (index >= filteredItems.length + 20) {
+      return null;
+    }
+
+    const nextPage = Math.floor(index / 20) + 1;
+    if (nextPage > page) {
+      setPage(nextPage);
+    }
+
+    if (!item && hasNextPage !== false) {
+      return (
+        <div style={style}>
+          <Card key={`loader-${rowIndex}-${columnIndex}`} loader>
+            <div
+              className={classes(
+                'absolute inset-0 z-10 bg-red-500',
+                isFetching ? 'hidden' : undefined
+              )}
+            />
+          </Card>
+        </div>
+      );
+    }
+
+    if (!item) {
+      return null;
+    }
+
+    const {
+      name,
+      price,
+      extra: { img },
+    } = item;
+
+    return (
+      <div style={style}>
+        <Card key={name} titleLeft={name} titleRight={price} loader>
+          <img
+            src={img}
+            alt={`Image for ${name}`}
+            className="absolute inset-0 z-10"
+          />
+        </Card>
+      </div>
+    );
+  };
+
+  const WINDOW_WIDTH = 832;
+  const WINDOW_HEIGHT = 888;
 
   return (
     <div className="space-y-4">
@@ -56,40 +89,17 @@ export const Listings: FunctionComponent = () => {
         />
       </div>
       <FixedSizeGrid
-        columnCount={1000}
-        columnWidth={100}
-        height={150}
-        rowCount={1000}
-        rowHeight={35}
-        width={300}
+        columnCount={COLUMNS}
+        columnWidth={Math.floor(WINDOW_WIDTH / COLUMNS)}
+        width={WINDOW_WIDTH}
+        rowCount={Math.ceil(
+          (filteredItems.length + (hasNextPage !== false ? 20 : 0)) / COLUMNS
+        )}
+        rowHeight={Math.floor(WINDOW_WIDTH / COLUMNS) + 56}
+        height={WINDOW_HEIGHT}
       >
         {Cell}
       </FixedSizeGrid>
-      {/*<Grid>*/}
-      {/*  {filteredItems.map(({ name, price, extra: { img } }) => (*/}
-      {/*    <Card key={name} titleLeft={name} titleRight={price} loader>*/}
-      {/*      <img*/}
-      {/*        src={img}*/}
-      {/*        alt={`Image for ${name}`}*/}
-      {/*        className="absolute inset-0 z-10"*/}
-      {/*      />*/}
-      {/*    </Card>*/}
-      {/*  ))}*/}
-      {/*  {hasNextPage !== false &&*/}
-      {/*    listEndRefs.map((ref, index) => (*/}
-      {/*      <Card key={index} loader>*/}
-      {/*        {index === 0 && ( // TODO*/}
-      {/*          <div*/}
-      {/*            ref={ref}*/}
-      {/*            className={classes(*/}
-      {/*              'absolute inset-0 z-10',*/}
-      {/*              isFetching ? 'hidden' : undefined*/}
-      {/*            )}*/}
-      {/*          />*/}
-      {/*        )}*/}
-      {/*      </Card>*/}
-      {/*    ))}*/}
-      {/*</Grid>*/}
     </div>
   );
 };
