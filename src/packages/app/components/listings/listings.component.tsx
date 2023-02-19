c873ec56ec4600e5fe9dd41aeb030e8cb08bb1a2
Sebastian Crow <sebastian@sebastiancrow.com>
2023-02-19 17:04:40 +0100

    react-window init

--- src/packages/app/components/listings/listings.component.tsx
+++ src/packages/app/components/listings/listings.component.tsx
@@ -5,7 +5,8 @@ import {
   RefObject,
   useMemo,
 } from 'react';
-import { Card, Grid, Input } from '../../../ui';
+import { FixedSizeGrid } from 'react-window';
+import { Card, Input } from '../../../ui';
 import { useFetchListings } from '../../hooks/useFetchListings';
 import { classes, useIntersectionObserver } from '../../../../shared';
 import { useFilterListings } from '../../hooks/useFilterListings';
@@ -39,6 +40,12 @@ export const Listings: FunctionComponent = () => {
     }
   }, [fetchNextPage, isFetching, listEnd]);
 
+  const Cell = ({ columnIndex, rowIndex, style }: any) => (
+    <div style={style}>
+      Item {rowIndex},{columnIndex}
+    </div>
+  );
+
   return (
     <div className="space-y-4">
       <div className="flex justify-center items-center">
@@ -48,31 +55,41 @@ export const Listings: FunctionComponent = () => {
           placeholder="Search NFT name"
         />
       </div>
-      <Grid>
-        {filteredItems.map(({ name, price, extra: { img } }) => (
-          <Card key={name} titleLeft={name} titleRight={price} loader>
-            <img
-              src={img}
-              alt={`Image for ${name}`}
-              className="absolute inset-0 z-10"
-            />
-          </Card>
-        ))}
-        {hasNextPage !== false &&
-          listEndRefs.map((ref, index) => (
-            <Card key={index} loader>
-              {index === 0 && ( // TODO
-                <div
-                  ref={ref}
-                  className={classes(
-                    'absolute inset-0 z-10',
-                    isFetching ? 'hidden' : undefined
-                  )}
-                />
-              )}
-            </Card>
-          ))}
-      </Grid>
+      <FixedSizeGrid
+        columnCount={1000}
+        columnWidth={100}
+        height={150}
+        rowCount={1000}
+        rowHeight={35}
+        width={300}
+      >
+        {Cell}
+      </FixedSizeGrid>
+      {/*<Grid>*/}
+      {/*  {filteredItems.map(({ name, price, extra: { img } }) => (*/}
+      {/*    <Card key={name} titleLeft={name} titleRight={price} loader>*/}
+      {/*      <img*/}
+      {/*        src={img}*/}
+      {/*        alt={`Image for ${name}`}*/}
+      {/*        className="absolute inset-0 z-10"*/}
+      {/*      />*/}
+      {/*    </Card>*/}
+      {/*  ))}*/}
+      {/*  {hasNextPage !== false &&*/}
+      {/*    listEndRefs.map((ref, index) => (*/}
+      {/*      <Card key={index} loader>*/}
+      {/*        {index === 0 && ( // TODO*/}
+      {/*          <div*/}
+      {/*            ref={ref}*/}
+      {/*            className={classes(*/}
+      {/*              'absolute inset-0 z-10',*/}
+      {/*              isFetching ? 'hidden' : undefined*/}
+      {/*            )}*/}
+      {/*          />*/}
+      {/*        )}*/}
+      {/*      </Card>*/}
+      {/*    ))}*/}
+      {/*</Grid>*/}
     </div>
   );
 };
