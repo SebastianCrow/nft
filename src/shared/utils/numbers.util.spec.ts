9cd2ef92ecf2aaa7dbe48eb072080965ee730ce5
Sebastian Crow <sebastian@sebastiancrow.com>
2023-02-21 19:26:20 +0100

    numbers.util spec

--- src/shared/utils/numbers.util.spec.ts
+++ src/shared/utils/numbers.util.spec.ts
@@ -3,10 +3,23 @@ import { truncatePositive } from './numbers.util';
 describe('numbers.util', () => {
   describe('truncatePositive', () => {
     it('truncates positive numbers correctly', () => {
-      expect(truncatePositive(1, 1).toString()).toStrictEqual('1');
-      expect(truncatePositive(1.2, 1).toString()).toStrictEqual('1.2');
-      expect(truncatePositive(1.21, 1).toString()).toStrictEqual('1.2');
-      expect(truncatePositive(1.29, 1).toString()).toStrictEqual('1.2');
+      expect(truncatePositive(100, 1)).toStrictEqual(100);
+
+      expect(truncatePositive(100.2, 1)).toStrictEqual(100.2);
+      expect(truncatePositive(100.21, 1)).toStrictEqual(100.2);
+      expect(truncatePositive(100.29, 1)).toStrictEqual(100.2);
+
+      expect(truncatePositive(100.2, 2)).toStrictEqual(100.2);
+      expect(truncatePositive(100.21, 2)).toStrictEqual(100.21);
+      expect(truncatePositive(100.29, 2)).toStrictEqual(100.29);
+
+      expect(truncatePositive(100.2, 3)).toStrictEqual(100.2);
+      expect(truncatePositive(100.21, 3)).toStrictEqual(100.21);
+      expect(truncatePositive(100.29, 3)).toStrictEqual(100.29);
+
+      expect(truncatePositive(100.2, 2)).toStrictEqual(100.2);
+      expect(truncatePositive(100.201, 2)).toStrictEqual(100.2);
+      expect(truncatePositive(100.299, 2)).toStrictEqual(100.29);
     });
 
     it('truncates zero correctly', () => {
