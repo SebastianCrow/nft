f3b7431376e128acea429b85e866f3bb8011b554
Sebastian Crow <sebastian@sebastiancrow.com>
2023-02-21 13:31:58 +0100

    Added clear icon to input

--- src/packages/ui/components/input/input.component.tsx
+++ src/packages/ui/components/input/input.component.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, FunctionComponent, useCallback } from 'react';
+import { MagnifyingGlassIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import { classes } from '../../../../shared';
 
 interface InputProps {
@@ -19,18 +20,53 @@ export const Input: FunctionComponent<InputProps> = ({
     [onValueChange]
   );
 
+  const clearValue = useCallback(() => {
+    onValueChange('');
+  }, [onValueChange]);
+
   return (
-    <input
-      value={value}
-      onChange={onChange}
-      placeholder={placeholder}
-      className={classes(
-        'py-3 px-6 w-full rounded-lg',
-        'transition-colors',
-        'focus:outline focus:dark:outline-dark',
-        'text dark:text-dark bg-highlight dark:bg-dark-highlight',
-        'placeholder:text-secondary dark:placeholder:text-dark-secondary'
-      )}
-    />
+    <div className="relative">
+      <input
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        className={classes(
+          'py-3 pl-6 pr-12 w-full rounded-lg',
+          'transition-colors',
+          'focus:outline focus:dark:outline-dark',
+          'text dark:text-dark bg-highlight dark:bg-dark-highlight',
+          'placeholder:text-secondary dark:placeholder:text-dark-secondary'
+        )}
+      />
+      <div
+        onClick={clearValue}
+        className={classes(
+          'absolute top-1/2 -translate-y-1/2 right-0',
+          'w-10 h-10',
+          'mx-1',
+          'flex justify-center items-center'
+        )}
+      >
+        {!value && (
+          <MagnifyingGlassIcon
+            className={classes(
+              'w-6',
+              'text-secondary dark:text-dark-secondary'
+            )}
+          />
+        )}
+        {value && (
+          <XMarkIcon
+            className={classes(
+              'w-7',
+              'cursor-pointer',
+              'transition-colors',
+              'text dark:text-dark',
+              'hover:text-secondary dark:hover:text-dark-secondary'
+            )}
+          />
+        )}
+      </div>
+    </div>
   );
 };
